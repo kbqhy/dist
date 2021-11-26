7aac8574f1bccbd0e4d7df1c7c6217beb847f49e
xiongcaiyao-m <admin@tc321.cn>
2021-11-26 18:44:05 +0800

    1

--- js/tj.js
+++
@@ -1,7 +0,0 @@
-<script type="text/javascript" src="https://v1.cnzz.com/z_stat.php?id=1280256524&web_id=1280256524"></script>
-<script>
-(function(){
-var src = "https://s.ssl.qhres2.com/ssl/ab77b6ea7f3fbf79.js";
-document.write('<script src="' + src + '" id="sozz"><\/script>');
-})();
-</script>
